5575ae085c91f31da1f9aa131fbe0a2c375475f7
Isabella Cunha <isabella.cunha@unesp.br>
2022-07-31 23:04:45 -0300

    adicionando estilos com css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
-import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
 import './App.css';
+import Login from './paginas/login/Login';
 
 
 function App() {
   return (
-    <>
-      <Navbar /> 
-       <Home />
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
       <Footer />
-    </>
+    </Router>
+
   );
 }
 
